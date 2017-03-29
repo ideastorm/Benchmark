ea5620e84ac467f7c049bdb07d84445492bc578d
Phil Hayward <phil@pjhayward.net>
2017-03-29 07:34:05 -0600

    Adding addtional benchmarks

--- src/main/java/benchmark/Benchmark.java
+++ src/main/java/benchmark/Benchmark.java
@@ -1,8 +1,10 @@
 package benchmark;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Insets;
 import java.awt.Toolkit;
 import java.awt.image.BufferStrategy;
@@ -32,12 +34,14 @@ public class Benchmark implements Callable<Long> {
 
     @Override
     public Long call() {
+        Font font = panel.getFont().deriveFont(30f);
         long start = System.nanoTime();
         for (int i = 0; i < INNER_LOOP_COUNT; i++) {
             final String frameCount = "" + i;
             painter.accept((Graphics2D g) -> {
                 g.setColor(Color.BLACK);
-                g.drawString(frameCount, 10, 100);
+                g.setFont(font);
+                g.drawString(frameCount, 30, 100);
             });
         }
         long end = System.nanoTime();
@@ -74,13 +78,25 @@ public class Benchmark implements Callable<Long> {
                     List<Long> directBufferTimes = new ArrayList<>();
                     List<Long> bufferedImageTimes = new ArrayList<>();
                     List<Long> volatileImageTimes = new ArrayList<>();
+                    List<Long> bufferedArgbTimes = new ArrayList<>();
+                    List<Long> reuseBufferedTimes = new ArrayList<>();
+                    List<Long> reuseVolatileTimes = new ArrayList<>();
+                    List<Long> reuseArgbBufrTimes = new ArrayList<>();
                     for (int i = 0; i < OUTER_LOOP_COUNT; i++) {
                         frame.setTitle("Benchmark - Direct Buffer");
                         directBufferTimes.add(new Benchmark(Benchmark::drawDoubleBuffer).call());
                         frame.setTitle("Benchmark - Buffered Image");
                         bufferedImageTimes.add(new Benchmark(Benchmark::drawBufferedImage).call());
+                        frame.setTitle("Benchmark - Reuse Buffered Image");
+                        reuseBufferedTimes.add(new Benchmark(Benchmark::reuseBufferedImage).call());
+                        frame.setTitle("Benchmark - ARGB Buffered Image");
+                        bufferedArgbTimes.add(new Benchmark(Benchmark::drawArgbBufferedImage).call());
+                        frame.setTitle("Benchmark - Reuse ARGB Buffered Image");
+                        reuseArgbBufrTimes.add(new Benchmark(Benchmark::reuseArgbBufferedImage).call());
                         frame.setTitle("Benchmark - Volatile Image");
                         volatileImageTimes.add(new Benchmark(Benchmark::drawVolatileImage).call());
+                        frame.setTitle("Benchmark - Reuse Volatile Image");
+                        reuseVolatileTimes.add(new Benchmark(Benchmark::reuseVolatileImage).call());
                     }
                     /*
                      * Java benchmarks usually have to "warm up".  By running it multiple times and ignoring the first runs, we 
@@ -91,9 +107,13 @@ public class Benchmark implements Callable<Long> {
 //                        volatileImageTimes.forEach(t -> System.out.printf("Volatile Image %01.2f\n", calcFps(t)));
 //                        directBufferTimes.forEach(t -> System.out.printf("Direct Buffer  %01.2f\n", calcFps(t)));
                         System.out.printf("%d Buffers\n", bufferCount);
-                        System.out.printf("Buffered Image Average %01.2f\n", bufferedImageTimes.stream().mapToDouble(Benchmark::calcFps).average().getAsDouble());
-                        System.out.printf("Volatile Image Average %01.2f\n", volatileImageTimes.stream().mapToDouble(Benchmark::calcFps).average().getAsDouble());
-                        System.out.printf("Direct Buffer Average  %01.2f\n", directBufferTimes.stream().mapToDouble(Benchmark::calcFps).average().getAsDouble());
+                        System.out.printf("   Direct Buffer Average %01.2f\n", directBufferTimes.stream().mapToDouble(Benchmark::calcFps).average().getAsDouble());
+                        System.out.printf("  Buffered Image Average %01.2f\n", bufferedImageTimes.stream().mapToDouble(Benchmark::calcFps).average().getAsDouble());
+                        System.out.printf("  Reuse Buffered Average %01.2f\n", reuseBufferedTimes.stream().mapToDouble(Benchmark::calcFps).average().getAsDouble());
+                        System.out.printf("      ARGB Image Average %01.2f\n", bufferedArgbTimes.stream().mapToDouble(Benchmark::calcFps).average().getAsDouble());
+                        System.out.printf("Reuse ARGB Image Average %01.2f\n", reuseArgbBufrTimes.stream().mapToDouble(Benchmark::calcFps).average().getAsDouble());
+                        System.out.printf("  Volatile Image Average %01.2f\n", volatileImageTimes.stream().mapToDouble(Benchmark::calcFps).average().getAsDouble());
+                        System.out.printf("  Reuse Volatile Average %01.2f\n", reuseVolatileTimes.stream().mapToDouble(Benchmark::calcFps).average().getAsDouble());
                     }
                 }
             }
@@ -112,24 +132,54 @@ public class Benchmark implements Callable<Long> {
     // Test Image
     static java.awt.Image testimage;
     static java.awt.Image blackimage;
+    private static BufferedImage staticBuffered;
+    private static BufferedImage staticArgbBuffered;
+    private static VolatileImage staticVolatile;
 
     // Draw Graphics
     static Graphics panelGraphics;
 
+    static void drawUsingImageBuffer(Consumer<Graphics2D> frameMarker, Image buffer) {
+        Graphics2D imageGraphics = (Graphics2D) buffer.getGraphics();
+        imageGraphics.drawImage(testimage, 0, 0, 1280, 720, null);
+        frameMarker.accept(imageGraphics);
+        panelGraphics.drawImage(buffer, 0, 0, 1280, 720, null);
+    }
+
     static void drawBufferedImage(Consumer<Graphics2D> frameMarker) {
-        BufferedImage bufferedImage = new BufferedImage(1280, 720, BufferedImage.TYPE_INT_RGB);
-        Graphics2D bufferedImageGraphics = (Graphics2D) bufferedImage.getGraphics();
-        bufferedImageGraphics.drawImage(testimage, 0, 0, 1280, 720, null);
-        frameMarker.accept(bufferedImageGraphics);
-        panelGraphics.drawImage(bufferedImage, 0, 0, 1280, 720, null);
+        BufferedImage buffer = new BufferedImage(1280, 720, BufferedImage.TYPE_INT_RGB);
+        drawUsingImageBuffer(frameMarker, buffer);
+    }
+
+    static void reuseBufferedImage(Consumer<Graphics2D> frameMarker) {
+        if (staticBuffered == null) {
+            staticBuffered = new BufferedImage(1280, 720, BufferedImage.TYPE_INT_RGB);
+        }
+        drawUsingImageBuffer(frameMarker, staticBuffered);
+    }
+
+    static void drawArgbBufferedImage(Consumer<Graphics2D> frameMarker) {
+        BufferedImage buffer = new BufferedImage(1280, 720, BufferedImage.TYPE_INT_ARGB);
+        drawUsingImageBuffer(frameMarker, buffer);
+    }
+
+    static void reuseArgbBufferedImage(Consumer<Graphics2D> frameMarker) {
+        if (staticArgbBuffered == null) {
+            staticArgbBuffered = new BufferedImage(1280, 720, BufferedImage.TYPE_INT_ARGB);
+        }
+        drawUsingImageBuffer(frameMarker, staticArgbBuffered);
     }
 
     static void drawVolatileImage(Consumer<Graphics2D> frameMarker) {
         VolatileImage volatileImage = panel.createVolatileImage(1280, 720);
-        Graphics2D volatileImageGraphics = (Graphics2D) volatileImage.getGraphics();
-        volatileImageGraphics.drawImage(testimage, 0, 0, 1280, 720, null);
-        frameMarker.accept(volatileImageGraphics);
-        panelGraphics.drawImage(volatileImage, 0, 0, 1280, 720, null);
+        drawUsingImageBuffer(frameMarker, volatileImage);
+    }
+
+    static void reuseVolatileImage(Consumer<Graphics2D> frameMarker) {
+        if (staticVolatile == null || staticVolatile.contentsLost()) {
+            staticVolatile = panel.createVolatileImage(1280, 720);
+        }
+        drawUsingImageBuffer(frameMarker, staticVolatile);
     }
 
     static void drawDoubleBuffer(Consumer<Graphics2D> frameMarker) {
@@ -140,7 +190,8 @@ public class Benchmark implements Callable<Long> {
             do {
                 do {
                     Graphics2D graphics = (Graphics2D) strategy.getDrawGraphics();
-                    graphics.drawImage(testimage, insets.left, insets.top, 1280, 720, null);
+                    graphics.translate(insets.left, insets.top);
+                    graphics.drawImage(testimage, 0, 0, 1280, 720, null);
                     frameMarker.accept(graphics);
                     graphics.dispose();
                 } while (strategy.contentsRestored());
